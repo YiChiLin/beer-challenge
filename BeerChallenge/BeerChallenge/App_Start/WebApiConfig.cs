83878e3a6b0f5ccefab15ee6dabbbbbdc75740ac
jean.lin52 <jean.lin52@TI03BY32>
2018-06-09 16:06:13 +0800

    added API router and basic UI render by Vue

---
+++ BeerChallenge/BeerChallenge/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace BeerChallenge
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.JsonFormatter.SerializerSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+        }
+    }
+}
---
+++ BeerChallenge/BeerChallenge/Controllers/Api/BeerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using BeerChallenge.Models;
+using BeerChallenge.Services;
+
+namespace BeerChallenge.Controllers.Api
+{
+    public class BeerController : ApiController
+    {
+        [HttpGet]
+        public async Task<List<BeerResponseDetail>> Beers(string beerName = "")
+        {
+            var service = new BreweryApiService();
+            var data = (await service.GetAllBeerInformationAsync()).Data;
+            return string.IsNullOrEmpty(beerName) ? data : data.Where(beer => beer.Name.Contains(beerName)).ToList();
+        }
+    }
+}

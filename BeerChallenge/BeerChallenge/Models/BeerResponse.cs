21fa1446e0e2864f6747ec867ca29a04513af623
jean.lin52 <jean.lin52@TI03BY32>
2018-06-10 10:02:40 +0800

    Added extra property for getting more information about each beer

--- BeerChallenge/BeerChallenge/Models/BeerResponse.cs
+++ BeerChallenge/BeerChallenge/Models/BeerResponse.cs
@@ -27,13 +27,58 @@ namespace BeerChallenge.Models
         [JsonProperty("ibu")]
         public string InternationalBitternessUnite { get; set; }
 
-        [JsonProperty("name")]
-        public string Name { get; set; }
+        [JsonProperty("isOrganic")]
+        public char IsOrganic { get; set; }
 
         [JsonProperty("nameDisplay")]
-        public string NameDisplay { get; set; }
+        public string Name { get; set; }
 
-        [JsonProperty("status")]
+        [JsonProperty("originalGravity")]
+        public string OriginalGravity { get; set; }
+
+        [JsonProperty("statusDisplay")]
         public string Status { get; set; }
+
+        [JsonProperty("style")]
+        public BeerStyle Style { get; set; }
+    }
+
+    public class BeerStyle
+    {
+        [JsonProperty("abvMax")]
+        public string AbvMax { get; set; }
+
+        [JsonProperty("abvMin")]
+        public string AbvMin { get; set; }
+
+        [JsonProperty("categoryId")]
+        public int CategoryId { get; set; }
+
+        [JsonProperty("fgMax")]
+        public string FgMax { get; set; }
+
+        [JsonProperty("fgMin")]
+        public string FgMin { get; set; }
+
+        [JsonProperty("ibuMax")]
+        public string IbuMax { get; set; }
+
+        [JsonProperty("ibuMin")]
+        public string IbuMin { get; set; }
+
+        [JsonProperty("ogMin")]
+        public string OgMin { get; set; }
+
+        [JsonProperty("srmMax")]
+        public string SrmMax { get; set; }
+
+        [JsonProperty("srmMin")]
+        public string SrmMin { get; set; }
+
+        [JsonProperty("description")]
+        public string StyleDescription { get; set; }
+
+        [JsonProperty("name")]
+        public string StyleName { get; set; }
     }
 }
